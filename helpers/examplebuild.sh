464723cb892571664caa7a454153c728ea02e5b9
Lilicogamer13 <brazilianmario@duck.com>
2026-07-17 07:43:15 -0300

    separate selfsigned generator from example selfsigned generator

--- helpers/examplebuild.sh
+++ helpers/examplebuild.sh
@@ -1,5 +1,6 @@
 export mainfolder=$(realpath .)
 mkdir $mainfolder/example_compiled
+openssl req -x509 -newkey rsa:4096 -keyout $mainfolder/example/key.pem -out $mainfolder/example/cert.pem -sha256 -days 365 -nodes -subj "/CN=localhost"
 gcc -Wall -Wextra -I"$mainfolder" example/simple.c src/http_server.c -o example_compiled/simple -lssl -lcrypto -lpthread
 gcc -Wall -Wextra -I"$mainfolder" example/filerouter.c src/http_server.c -o example_compiled/filerouter -lssl -lcrypto -lpthread
 gcc -Wall -Wextra -I"$mainfolder" example/simple_https.c src/http_server.c -o example_compiled/simple_https -lssl -lcrypto -lpthread
--- helpers/genselfsignedssl.sh
+++ helpers/genselfsignedssl.sh
@@ -1,1 +1,1 @@
-openssl req -x509 -newkey rsa:4096 -keyout example/key.pem -out example/cert.pem -sha256 -days 365 -nodes -subj "/CN=localhost"
+openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -sha256 -days 365 -nodes -subj "/CN=localhost"
